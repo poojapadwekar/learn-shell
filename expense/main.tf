67def6cf786d192d9f1e2958b95ba027635f8266
sauja <pkpadwekar21@gmail.com>
2023-09-26 22:00:55 +0530

    Practice1

--- expense/main.tf
+++ expense/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "frontend1" {
 }
 
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "frontend1" {
   zone_id = "Z00236712TJAPP7J1TL57"
   name    = "frontend1.saujpoo.online"
   type    = "A"
@@ -27,7 +27,7 @@ resource "aws_instance" "backend1" {
 }
 
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "backend1" {
   zone_id = "Z00236712TJAPP7J1TL57"
   name    = "backend1.saujpoo.online"
   type    = "A"
@@ -46,7 +46,7 @@ resource "aws_instance" "mysql1" {
 }
 
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "mysql1" {
   zone_id = "Z00236712TJAPP7J1TL57"
   name    = "mysql1.saujpoo.online"
   type    = "A"
