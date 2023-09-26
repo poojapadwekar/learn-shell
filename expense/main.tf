20db1c02e048b3add1428e200673581516a13fa5
sauja <pkpadwekar21@gmail.com>
2023-09-26 23:09:06 +0530

    Practice1

--- expense/main.tf
+++ expense/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0b3eb5c937970df48"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend1"
@@ -21,7 +21,7 @@ resource "aws_route53_record" "frontend1" {
 resource "aws_instance" "backend1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0b3eb5c937970df48"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "backend1"
@@ -41,7 +41,7 @@ resource "aws_route53_record" "backend1" {
 resource "aws_instance" "mysql1" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0b3eb5c937970df48"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "mysql1"
