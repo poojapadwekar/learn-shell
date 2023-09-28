bd4ea6958b6d91b0d51d3911a67c02ca193afc93
sauja <pkpadwekar21@gmail.com>
2023-09-28 20:56:59 +0530

    Practice1

--- expense/main.tf
+++ expense/main.tf
@@ -7,23 +7,26 @@ resource "aws_instance" "frontend1" {
     Name = "frontend"
   }
 
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/infra-ansible
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
-EOF
+  resource "aws_route53_record" "frontend1" {
+    zone_id = data.aws_route53_zone.route.zone_id
+    name    = "frontend.${var.zone_id}"
+    type    = "A"
+    ttl     = 30
+    records = [aws_instance.frontend1.private_ip]
+  }
+
+  resource "null_resource" "frontend" {
+    provisioner "local-exec" {
+      command = <<EOF
+  cd /home/centos/infra-ansible
+  git pull
+  sleep 60
+  ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+  EOF
   }
 }
 
-resource "aws_route53_record" "frontend1" {
-  zone_id = data.aws_route53_zone.route.zone_id
-  name    = "frontend.${var.zone_id}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend1.private_ip]
-}
+
 
 
 resource "aws_instance" "backend1" {
@@ -34,6 +37,16 @@ resource "aws_instance" "backend1" {
   tags = {
     Name = "backend"
   }
+
+resource "aws_route53_record" "backend1" {
+  zone_id = data.aws_route53_zone.route.zone_id
+  name    = "backend1.${var.zone_id}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend1.private_ip]
+}
+
+  resource "null_resource" "backend" {
    provisioner "local-exec" {
      command = <<EOF
 cd /home/centos/infra-ansible
@@ -45,13 +58,7 @@ EOF
 }
 
 
-resource "aws_route53_record" "backend1" {
-  zone_id = data.aws_route53_zone.route.zone_id
-  name    = "backend1.${var.zone_id}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.backend1.private_ip]
-}
+
 
 
 resource "aws_instance" "mysql1" {
@@ -63,6 +70,16 @@ resource "aws_instance" "mysql1" {
     Name = "mysql"
   }
 
+
+resource "aws_route53_record" "mysql1" {
+  zone_id = data.aws_route53_zone.route.zone_id
+  name    = "mysql1.${var.zone_id}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql1.private_ip]
+}
+
+resource "null_resource" "mysql" {
   provisioner "local-exec" {
     command = <<EOF
 cd /home/centos/infra-ansible
@@ -74,10 +91,3 @@ EOF
 }
 
 
-resource "aws_route53_record" "mysql1" {
-  zone_id = data.aws_route53_zone.route.zone_id
-  name    = "mysql1.${var.zone_id}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql1.private_ip]
-}
